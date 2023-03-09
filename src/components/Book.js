10c2b36286d1234b434e9713c1a88cdf17e27296
shaaibu <shaaibusuleiman@gamil.com>
2023-03-09 21:11:33 +0100

    Complete styling for Bookstore

--- src/components/Book.js
+++ src/components/Book.js
@@ -17,10 +17,37 @@ const Book = ({
   }
   return (
     <div className={styles.item}>
+      <div className={styles.display}>
+      <div className={styles.info}>
       <p>{category}</p>
-      <h3>{title}</h3>
+      <h3 className={styles.bookName}>{title}</h3>
       <p>{author}</p>
+      </div>
+      <div className={styles.btns}>
+      <button type='button'>Comments</button>
+      <div className={styles.gery}></div>
       <button type="button" data-id={id} onClick={removeBookHandler}>Remove</button>
+      <div className={styles.gery}></div>
+      <button type='button'>Edit</button>
+      </div>
+      </div>
+
+      <div className={styles.contain}>
+        <div className={styles.ovalContainer}>
+          <div className={styles.Oval} />
+        </div>
+        <div>
+          <div className={styles.completion}>98%</div>
+          <div className={styles.completed}>completed</div>
+        </div>
+      </div>
+
+      <div className={styles.bar}></div>
+      <div className={styles.chapterContainer}>
+        <div className={styles.chapter}>Current Chapter</div>
+        <div className={styles.chapter}>Chapter Twelve</div>
+        <div className={styles.chapterBtn}>Update Progress</div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import styles from './styles/Navbar.module.css';
 
 const Navigation = () => (
   <header className={styles.header}>
-    <h1>Bookstore CMS</h1>
-    <nav className={styles.nav}>
-      <NavLink to="/">BOOKS</NavLink>
-      <NavLink to="/categories">CATEGORIES</NavLink>
-    </nav>
+    <div className={styles.head}>
+      <h1>Bookstore CMS</h1>
+      <nav className={styles.nav}>
+        <NavLink to="/">BOOKS</NavLink>
+        <NavLink to="/categories">CATEGORIES</NavLink>
+      </nav>
+    </div>
+    <div className={styles.user}>
+      <button type="button">
+        <FaUser />
+      </button>
+    </div>
     <Outlet />
   </header>
 );
